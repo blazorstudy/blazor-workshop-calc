a02ede1914719f3a67d97ed46ba54640a21298c1
zihyeon08 <104641422+zihyeon08@users.noreply.github.com>
2024-04-23 19:40:03 +0900

    주석제거

--- src/BlazorCalc/BlazorCalc/Program.cs
+++ src/BlazorCalc/BlazorCalc/Program.cs
@@ -2,20 +2,16 @@ using BlazorCalc.Components;
 using BlazorCalc.Components.Calc;
 
 var builder = WebApplication.CreateBuilder(args);
-// API 연결을 위해서 HttpClient 사용하기 위해 HttpClient 서비스 추가.
 
 builder.Services.AddHttpClient();
 
-// Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-// CalcState 추가
 builder.Services.AddSingleton<CalcState>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
